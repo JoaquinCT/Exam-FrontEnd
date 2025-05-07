d5af114922d0ee00652a45a6393440b8bdf0f1a7
JoaquinCT <jose.cortes.tenorio@est.una.ac.cr>
2025-05-06 23:28:21 -0600

    Creation of the base app and the models

---
+++ ExamFrontEnd/app/src/main/java/com/una/exam_frontend/models/Student.kt
@@ -0,0 +1,8 @@
+package com.una.exam_frontend.models
+
+data class Student(
+    val id: Int,
+    val name: String,
+    val email: String,
+    val phone: String
+)
---
+++ ExamFrontEnd/app/src/main/java/com/una/exam_frontend/network/RetrofitInstance.kt
@@ -0,0 +1,14 @@
+package com.una.exam_frontend.network
+import com.una.exam_frontend.common.Constants.API_BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
