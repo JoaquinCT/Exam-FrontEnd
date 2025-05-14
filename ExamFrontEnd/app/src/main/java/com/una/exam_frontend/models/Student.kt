6b31cb2da6d36a1bcbbb87069eaff52aee679aaf
JoaquinCT <jose.cortes.tenorio@est.una.ac.cr>
2025-05-14 15:44:23 -0600

    Fix student.kt foreing key issue

--- ExamFrontEnd/app/src/main/java/com/una/exam_frontend/models/Student.kt
+++ ExamFrontEnd/app/src/main/java/com/una/exam_frontend/models/Student.kt
@@ -5,11 +5,20 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity (tableName = "Students")
+@Entity(
+    tableName = "Students",
+    foreignKeys = [ForeignKey(
+        entity = Course::class,            // Entidad referenciada
+        parentColumns = ["id"],            // Columna en Course (PK)
+        childColumns = ["course_id"],      // Columna en Student (FK)
+        onDelete = ForeignKey.CASCADE      // Acción al eliminar curso
+    )]
+)
 data class Student(
     @PrimaryKey val id: Int,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "email") val email: String,
     @ColumnInfo(name = "phone") val phone: String,
-    @ForeignKey val courseId: Int
+    @ColumnInfo(name = "course_id") val courseId: Int  // FK con Course
 )
+
